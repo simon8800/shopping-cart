38d308e6ec5e4b7721d9ca1aa5b86f4783db7b56
Simon Mei <smei8800@gmail.com>
2024-08-22 00:22:06 -0400

    Create Button with styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,68 @@
 import styled from "styled-components";
 
+const buttonStyles = {
+  default: {
+    background: '#9764c7',
+    color: '#ffffff',
+    border: '#9764c7',
+    hoverBackground: '#7646A6',
+    hoverBorder: '#7646A6',
+  },
+  primary: {
+    background: '#007bff',
+    color: '#ffffff',
+    border: '#007bff',
+    hoverBackground: '#0056b3',
+    hoverBorder: '#0056b3',
+  },
+  secondary: {
+    background: '#6c757d',
+    color: '#ffffff',
+    border: '#6c757d',
+    hoverBackground: '#5a6268',
+    hoverBorder: '#545b62',
+  },
+  success: {
+    background: '#28a745',
+    color: '#ffffff',
+    border: '#28a745',
+    hoverBackground: '#218838',
+    hoverBorder: '#1e7e34',
+  },
+  danger: {
+    background: '#dc3545',
+    color: '#ffffff',
+    border: '#dc3545',
+    hoverBackground: '#c82333',
+    hoverBorder: '#bd2130',
+  },
+  warning: {
+    background: '#ffc107',
+    color: '#212529',
+    border: '#ffc107',
+    hoverBackground: '#e0a800',
+    hoverBorder: '#d39e00',
+  },
+};
+
 const Button = styled.button`
-  background: white;
+  display: inline-block;
+  color: ${props => (props.$variation ? buttonStyles[props.$variation].color: buttonStyles.default.color)};;
+  background: ${props => (props.$variation ? buttonStyles[props.$variation].background: buttonStyles.default.background)};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid #BF4F74;
+  border: 2px solid ${props => (props.$variation ? buttonStyles[props.$variation].border: buttonStyles.default.border)};;
   border-radius: 3px;
+  cursor: pointer;
+  text-style: normal;
+  text-decoration: none;
+  transition: background-color 0.3s ease, border 0.3s ease;
+
+  &:hover {
+    background: ${props => (props.$variation ? buttonStyles[props.$variation].hoverBackground: buttonStyles.default.hoverBackground)};;
+    border-color: ${props => (props.$variation ? buttonStyles[props.$variation].hoverColor: buttonStyles.default.hoverColor)};;
+  }
 `;
 
-const TomatoButton = styled(Button)`
-  color: tomato;
-  border-color: tomato;
-`
-
-export {Button, TomatoButton};
+export default Button;
