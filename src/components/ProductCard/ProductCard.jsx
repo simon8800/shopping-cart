3847a1b015e1f499edb4dc97fd1644dac1560a30
Simon Mei <smei8800@gmail.com>
2024-09-01 23:50:17 -0400

    Styling ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,6 +4,7 @@ import { useOutletContext } from 'react-router-dom';
 // style components
 import { FlexContainer } from '../Basic/Flex';
 import { ProductCardContainer, ImageWrapper, CardImage, ProductTitleWrapper} from "./ProductCardStyles"
+import { StarIcon } from 'hugeicons-react';
 import Button from "../Basic/Button";
 
 const ProductCard = ({product}) => {
@@ -19,7 +20,7 @@ const ProductCard = ({product}) => {
           <h3 style={{fontSize: "17px", fontWeight: "400"}}>{product.title}</h3>
         </ProductTitleWrapper>
         <p>{product.price}</p>
-        <p>⭐️{product.rating.rate} {product.rating.count}</p>
+        <p><StarIcon size={16} color={"#000000"} variant={"stroke"}/>{product.rating.rate} ({product.rating.count})</p>
         <Button>Add to cart</Button>
       </FlexContainer>
     </ProductCardContainer>
--- src/components/ProductCard/ProductCardStyles.jsx
+++ src/components/ProductCard/ProductCardStyles.jsx
@@ -1,5 +1,6 @@
 // modules
 import styled from "styled-components";
+import devices from '../../utils/devices';
 
 // 294 x 392 (w x h)
 const CardWidth = "294px";
@@ -7,14 +8,17 @@ const CardWidth = "294px";
 const ProductCardContainer = styled.div`
   display: inline-block;
   max-width: ${CardWidth};
-  outline: 1px solid black;
+  outline: 1px solid red;
   background-color: #FFFFFF;
+
+  @media only screen and ${devices.sm} {
+    max-width: 200px;
+  }
 `
 
 const ImageWrapper = styled.div`
   display: flex;
   max-width: ${CardWidth};
-  height: 392px;
   outline: 1px solid black;
 `
 
@@ -25,19 +29,23 @@ const CardImage = styled.img`
 `
 
 const ProductTitleWrapper = styled.div`
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  height: 2.8rem;
-  overflow: hidden;
-  text-overflow: ellipsis;
+
 `
+
+// const ProductTitleWrapper = styled.div`
+//   display: -webkit-box;
+//   -webkit-line-clamp: 2;
+//   -webkit-box-orient: vertical;
+//   height: 2.8rem;
+//   overflow: hidden;
+//   text-overflow: ellipsis;
+// `
 
 // const ProductDetailsContainer = styled.div`
 //   display: -webkit-box;
 //   -webkit-line-clamp: 3;
 //   -webkit-box-orient: vertical;
-//   width: ${cardWidth};
+//   width: ${CardWidth};
 //   height: 4rem;
 //   overflow: hidden;
 //   text-overflow: ellipsis;
